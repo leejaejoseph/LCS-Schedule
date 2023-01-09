a3548bfbc492074c1c3ded5e231d4c9441a069e2
Joseph Youngjae Lee <116613246+leejaejoseph@users.noreply.github.com>
2023-01-09 23:52:20 +0000

    Create HTML Layout for top navbar. Make the Teams Button in Navbar link to the Teams page. Setup layout of page with three rows for the H1, 10 team clickers, and selected teams button. Write CSS for mobile devices first. #1

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var $teamIcon = document.querySelector('.team-icon');
+$teamIcon.addEventListener('click', function () {
+  var $clicker = document.querySelector('.clicker');
+  $clicker.classList.add('active');
+}
+);
