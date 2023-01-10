c3bfe85447f5617c1847eef2571a68381a572ebe
Joseph Youngjae Lee <116613246+leejaejoseph@users.noreply.github.com>
2023-01-10 18:49:16 +0000

    For each button, Add a 'click' event listener that:
        changes the background-color of the selected icon:
            turns on when it was off.
            turns off when it was on.
        stores the selected teams into the data model object if button is turned on
        removes selected teams in the data model object if button is turned off. #1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,31 @@
-var $teamIcon = document.querySelector('.team-icon');
-$teamIcon.addEventListener('click', function () {
-  var $clicker = document.querySelector('.clicker');
-  $clicker.classList.add('active');
+var $teamIcon = document.querySelectorAll('.team');
+for (var i = 0; i < $teamIcon.length; i++) {
+  $teamIcon[i].classList.add('team' + i);
+  var $teamIconNum = document.querySelector('.team' + i);
+  $teamIconNum.addEventListener('click', lights);
 }
-);
+
+function lights(event) {
+  var $clicker = event.target.closest('.team-icon').querySelector('.clicker');
+  if (!$clicker.classList.contains('active')) {
+    $clicker.classList.add('active');
+    addSelected(event.target.closest('.team-wrapper').id);
+  } else {
+    $clicker.classList.remove('active');
+    removeSelected(event.target.closest('.team-wrapper').id);
+  }
+}
+
+function addSelected(teamId) {
+  data.selected.push(teamId);
+}
+
+function removeSelected(teamId) {
+  var index;
+  for (var i = 0; i < data.selected.length; i++) {
+    if (data.selected[i] === teamId) {
+      index = i;
+    }
+  }
+  data.selected.splice(index, 1);
+}
